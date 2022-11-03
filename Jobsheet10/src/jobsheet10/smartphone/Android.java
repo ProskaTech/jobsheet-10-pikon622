cc4b42ebf94f590e5faa3acf1867b43b707d789d
Muhammad Farras Fiqhan <112669349+pikon622@users.noreply.github.com>
2022-11-03 21:48:36 +0700

    Add files via upload

---
+++ Jobsheet10/src/jobsheet10/smartphone/Android.java
@@ -0,0 +1,24 @@
+package jobsheet10.smartphone;
+
+/**
+ *
+ * Created by Pikonnn
+ */
+
+public class Android {
+    public void nyala(){
+        System.out.println("Android menyala");
+    }
+    
+    public void panggilan(){
+        System.out.println("Kring Kring Kring, ada panggilan masuk");
+    }
+    
+    public void sms(){
+        System.out.println("Tenenenoeeett, ada SMS baru");
+    }
+    
+    public void shutdown(){
+        System.out.println("Android dimatikan");
+    }
+}
